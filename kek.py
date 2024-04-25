c8f958f398cbd3e9240749317591398010dad9c8
Rekvizit <73797503@mail.ru>
2024-04-25 11:47:33 +0000

    upload

---
+++ kek.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True)
+    password = Column(String)
+
+engine = create_engine('sqlite:///users.db')
+Base.metadata.create_all(engine)
