f960fd67782d864dbfe4be06503b0a7557f547fa
Rekvizit <73797503@mail.ru>
2024-05-02 19:55:38 +0300

    update

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from fastapi import FastAPI, Request, Form, Depends, HTTPException
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from passlib.context import CryptContext
 from database import User
+from itsdangerous import URLSafeTimedSerializer
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 db = User("users.db")
 
 app = FastAPI()
+
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+SECRET_KEY = 'secret_key'
+session_serializer = URLSafeTimedSerializer(SECRET_KEY)
+
+def remove_session(response: RedirectResponse):
+    response.delete_cookie('session')
+    return response
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
@@ -35,14 +46,23 @@ def login(request: Request):
 @app.post("/login")
 def login_user(request: Request, username: str = Form(...), password: str = Form(...)):
     if db.is_existence(username) and pwd_context.verify(password, db.get_hash_password(username)):
-        return templates.TemplateResponse("profile.html", {"request": request, "username": username})
+        response = templates.TemplateResponse("profile.html", {"request": request, "username": username})
+        session_token = session_serializer.dumps(username)
+        response.set_cookie('session', session_token)
+        return response
     else:
         raise HTTPException(status_code=400, detail="Invalid username or password")
 
 @app.get("/profile", response_class=HTMLResponse)
-def profile(request: Request, user_id: int):
-    username = db.get_name_by_id(user_id)
-    if username:
+def profile(request: Request, username: str):
+    session_token = request.cookies.get('session')
+    if session_token:
+        username = session_serializer.loads(session_token, max_age=86400)
         return templates.TemplateResponse("profile.html", {"request": request, "username": username})
     else:
-        raise HTTPException(status_code=404, detail="User not found")
+        return RedirectResponse(url='/login')
+
+@app.get("/logout")
+def logout():
+    response = RedirectResponse(url='/login')
+    return remove_session(response)
